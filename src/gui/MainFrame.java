b0552be834c53933d53039fee3d23f40019a7abe
Mathias Van Herreweghe <mathias.van.herreweghe@hotmail.com>
2017-11-28 16:18:10 +0100

    tmp

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -45,6 +46,10 @@ public class MainFrame extends JFrame implements ActionListener {
 			int returnVal = fc.showOpenDialog(MainFrame.this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				file = fc.getSelectedFile();
+				try {
+					Runtime.getRuntime().exec("ffmpeg -i file.wav -f -s16be -ar 8000 -acodec pcm_s16be file.raw", null, Context.getFilesDir());
+				} catch (IOException exc) {
+				}
 				//String filePath = file.getAbsolutePath();
 			} else {
 				// cancel
