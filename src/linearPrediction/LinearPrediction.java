3e425963b93d3678ff19fa1b3121bc323db4f020
Robo 2.0 <t.vempati268@gmail.com>
2018-01-29 03:24:56 +0100

    latest added LPC

--- src/linearPrediction/LinearPrediction.java
+++ src/linearPrediction/LinearPrediction.java
@@ -3,12 +3,13 @@ import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import linearPrediction.DiscreteAutocorrelation;
+import utils.Util;
 
 /**
  * Linear Predictive Coding algorithm
  * <p>
  * Since this algorithm is generally used in an iterative process over small windows of audio data, 
- * it's output reuses the same buffers over and over again in order to avoid memory allocation 
+ * it's PedictionOutput reuses the same buffers over and over again in order to avoid memory allocation 
  * and garbage collection alltogether. It's up to the client to copy the returned data should it 
  * want to keep it as-is and not use it in further processing.
  * </p>
@@ -22,10 +23,9 @@ public class LinearPrediction {
 
     private final int windowSize;
     private final int poles;
-    private final double[] output;
-    private final double[] error;
-    private final double[] k;
-    private final double[][] matrix;
+    private final double[] PedictionOutput;
+    private final double[] PedictionError;
+    
     
 
     /**
@@ -36,10 +36,9 @@ public class LinearPrediction {
     public LinearPrediction(int windowSize, int poles) {
         this.windowSize = windowSize;
         this.poles = poles;
-        this.output = new double[poles];
-        this.error = new double[poles];
-        this.k = new double[poles];
-        this.matrix = new double[poles][poles];
+        this.PedictionOutput = new double[poles];
+        this.PedictionError = new double[poles];
+       
         
 
     }
@@ -48,74 +47,61 @@ public class LinearPrediction {
      * Apply Linear Predictive Coding 
      * 
      * @param window windowed part of voice sample, must be of the same size as the windowSize passed in constructor
-     * @return an array of size 2 containing LPC coefficients in 0 and error coefficients in 1
+     * @return an array of size 2 containing LPC coefficients in 0 and PedictionError coefficients in 1
      */
     public double[][] applyLinearPredictiveCoding(double[] window) {
+    	System.out.println(windowSize+"WSSSS"+window.length+"wllll");
         
         if(windowSize != window.length) {
             throw new IllegalArgumentException("Given window length was not equal to the one provided in constructor : [" 
                     + window.length +"] != [" + windowSize + "]");
         }
         
-        Arrays.fill(k,  0.0d);
-        Arrays.fill(output, 0.0d);
-        Arrays.fill(error, 0.0d);
-        for(double[] d : matrix) {
-            Arrays.fill(d, 0.0d);
-        }
+        
+        Arrays.fill(PedictionOutput, 0.0d);
+        Arrays.fill(PedictionError, 0.0d);
+       
 
         DiscreteAutocorrelation dalj = new DiscreteAutocorrelation();
         double[] autocorrelations = new double[poles];
         for(int i = 0; i < poles; i++) {
         
-           // autocorrelations[i] = dalj.autocorrelate(window, i);
-        	autocorrelations[i] = dalj.Myautocorrelate(window[i], window[i+1]);
-        	System.out.println(autocorrelations[i]);
-        }
-              
-        
-
-        error[0] = autocorrelations[0];
-      
-
-        for (int m = 1; m < poles; m++) {
-            double tmp = autocorrelations[m];
-            for (int i = 1; i < m; i++) {
-                tmp -= matrix[m - 1][i] * autocorrelations[m - i];
-                
-            }
-            k[m] = tmp / error[m - 1];
-
-            for (int i = 0; i < m; i++) {
-                matrix[m][i] = matrix[m - 1][i] - k[m] * matrix[m - 1][m - i];
-            }
-            matrix[m][m] = k[m];
-            error[m] = (1 - (k[m] * k[m])) * error[m - 1];
-            
-           
-        }
-
-       for (int i = 0; i < poles; i++) {
-            if (Double.isNaN(matrix[poles - 1][i])) {
-                output[i] = 0.0;
-            } else {
-                output[i] = matrix[poles - 1][i];
-            }  
+        //   autocorrelations[i] = dalj.autocorrelate(window, i);
+       	autocorrelations[i] = dalj.Myautocorrelate(window[i], window[i+1]);
+       	System.out.println(autocorrelations[i]+"autoii");
         }
         
+        PedictionOutput[0]=0.0;
+        PedictionOutput[1]=window[0];
+        PedictionError[0]=window[0];
        
+      //  PedictionError[0] = autocorrelations[0];
+        for (int m = 1; m < poles; m++) {
+            PedictionError[m] = autocorrelations[m-1];
+             }
+     
+       for(int j=2;j<poles;j++) {
+    	   PedictionOutput[j] = PedictionError[j-1]+PedictionOutput[j-1];
+       }
+//   Testing purpose
+       //for(int h=0;h<poles;h++) {
+//    	   System.out.println(PedictionOutput[h]+"outhhhh");
+//       }
+  
+      
         
-        return new double[][] { output, error };
+        return new double[][] { PedictionOutput, PedictionError };
     }
     
-    public static void main(String[] args) {
-    	double [] myList = {2,7,5,9,17};
+  /*Testing purpose
+   * 
+   *  public static void main(String[] args) {
+    	double [] myList = {1,5,4,7};
   
-    	 LinearPrediction lp = new LinearPrediction(5,4);
-    	lp.applyLinearPredictiveCoding(myList);
-    	
+    	 LinearPrediction lp = new LinearPrediction(4,3);
+    lp.applyLinearPredictiveCoding(myList);
     	
 		
-	}
+	}*/
    
 }
